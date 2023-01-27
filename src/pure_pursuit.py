ba7500ec165c9329946fe587950fdb7d78960ef0
ParkJuneHong <junhp1234@cu.ac.kr>
2023-01-27 23:09:50 +0900

    Pure pursuit code update (working)

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -37,38 +37,82 @@ class PurePursuit:
         
         refined_closest_point = [closest_point.x, closest_point.y]
 
-        lookahead_point = refined_closest_point + self.lookahead_distance * (closest_current_sub) / closest_distance
+        closest_distance_list = [closest_distance, closest_distance]
+        lookahead_distance_list = [self.lookahead_distance, self.lookahead_distance]
+        
+        distance_mul = [lookahead_distance_list[i] * closest_current_sub[i] for i in range(len(closest_current_sub))]
+        
+        distance_div = [distance_mul[i] / closest_distance_list[i] for i in range(len(closest_distance_list))]
+
+        # lookahead_point = refined_closest_point + (self.lookahead_distance * (closest_current_sub) / closest_distance) # Original code
+        lookahead_point = [refined_closest_point[i] + distance_div[i] for i in range(len(distance_div))]
+        
+        #print()
+        #print("refined_closest_point")
+        #print(refined_closest_point)
+        #print("distance_div")
+        #print(distance_div)
+        #print("self.lookahead_distance")
+        #print(self.lookahead_distance)
+        #print("closest_current_sub")
+        #print(closest_current_sub)
+        #print("closest_distance")
+        #print(closest_distance)
+        #print("lookahead_point")
+        #print(lookahead_point)
 
         # Calculate the steering angle
         steering_angle = np.arctan2(lookahead_point[1] - current_pose[1], lookahead_point[0] - current_pose[0]) - current_pose[2]
 
         # Limit the steering angle
         steering_angle = np.clip(steering_angle, -self.max_steering_angle, self.max_steering_angle)
-        print("steering_angle")
-        print(steering_angle)
 
         return steering_angle
 
 class Controller:
     def __init__(self):
         print("Controller __init__ called.")
-        self.path = []
-        self.pure_pursuit = PurePursuit(1, 3)
+        self.path = None
+        self.pure_pursuit = PurePursuit(2, 0.5)
         self.pub = rospy.Publisher("/ctrl_cmd", CtrlCmd, queue_size=1)
+        '''
+        int32 longlCmdType
+        float64 accel
+        float64 brake
+        float64 steering # positive : left / negative : right
+        
+        float64 velocity
+        float64 acceleration
+        '''
         self.path_sub = rospy.Subscriber("/refined_vertices", MarkerArray, self.path_callback)
         self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         rospy.spin()
         
     def path_callback(self, msg):
-        print("path_callback called.")
+        print("Path updated.")
         self.path = msg.markers
+        # print(self.path)
+        #for marker in self.path:
+            #print(marker.points[0].x)
+            #print(marker.points[0].y)
         
     def odom_callback(self, msg):
-        print("odom_callback called.")
-        current_pose = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.orientation.z]
-        path_points = [Point(p.pose.position.x, p.pose.position.y, 0.0) for p in self.path]
-        steering_angle = self.pure_pursuit.calculate_steering_angle(current_pose, path_points)
-        self.pub.publish(steering_angle)
+        cmd = CtrlCmd()
+        if self.path == None:
+            pass
+        else:
+            current_pose = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.orientation.z]
+            # path_points = [Point(p.pose.position.x, p.pose.position.y, 0.0) for p in self.path] # Original code
+            path_points = [Point(p.points[0].x, p.points[0].y, 0.0) for p in self.path]
+            # print(current_pose)
+            # print(path_points)
+            steering_angle = self.pure_pursuit.calculate_steering_angle(current_pose, path_points)
+        
+            cmd.accel = 0.35
+            cmd.steering = steering_angle
+            # print(steering_angle)
+        
+            self.pub.publish(cmd)
         
 if __name__=="__main__":
         rospy.init_node("Pure_pursuit")
