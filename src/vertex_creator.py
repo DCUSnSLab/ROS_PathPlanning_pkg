a4dfd1a15fcad08cabca22a0eb2e20827e38ff40
ParkJuneHong <junhp1234@cu.ac.kr>
2023-08-24 17:07:30 +0900

    Connected vertex(Vertices which closer than threshold) now display Line list in Rviz

--- src/vertex_creator.py
+++ src/vertex_creator.py
@@ -6,8 +6,9 @@ import json
 import time
 import numpy as np
 from std_msgs.msg import Int32
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from geometry_msgs.msg import Point, PoseStamped, PoseWithCovarianceStamped
 from visualization_msgs.msg import Marker, MarkerArray
+from std_msgs.msg import Header
 from nav_msgs.msg import Path
 
 '''
@@ -19,15 +20,39 @@ class Path_creator():
         self.goal_sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.callback)
         self.end_sub = rospy.Subscriber('/end', Int32, self.end)
         self.path_pub = rospy.Publisher('/current_vertices', Path, queue_size=1)
+        self.adjacent_pub = rospy.Publisher('/vertices_adjacents', Marker, queue_size=1)
 
         self.thrsh = 2.5 # threshold for connect each vertex
         self.pose_list = list()
         self.path = Path()
+        self.path_adjacent = Marker()
+
+        # Marker type and style define
+        self.path_adjacent.header = self.make_header("map")
+        self.path_adjacent.type = 5
+        self.path_adjacent.scale.x = 0.5
+        self.path_adjacent.color.r = 0.5
+        self.path_adjacent.color.g = 0.5
+        self.path_adjacent.color.b = 1.0
+        self.path_adjacent.color.a = 1.0
+
+        # sample code for Marker(LINE_LIST)
+        #self.path_adjacent.points.append(Point(x=0, y=0))
+        #self.path_adjacent.points.append(Point(x=5, y=5))
+
         self.prev_pose = None
         self.vertex_count = 0
 
         self.coordinates_list = list()
         rospy.on_shutdown(self.shutdown)
+
+    def make_header(self, frame_id, stamp=None):
+        if stamp == None:
+            stamp = rospy.Time.now()
+        header = Header()
+        header.stamp = stamp
+        header.frame_id = frame_id
+        return header
 
     def callback(self, goal):
         self.path.header.stamp = goal.header.stamp
@@ -48,20 +73,21 @@ class Path_creator():
             # print(self.vertex_count)
             # print(str(self.vertex_count))
             each_coord = [d["xy"] for d in self.pose_list]
-            print("each_coord")
-            print(each_coord)
-            print()
             np_each_coord = np.array(each_coord)
             each_distances = np.linalg.norm(np_each_coord - (x, y), axis=1)
-            print(each_distances)
             below_thrsh_indices = np.where(each_distances < self.thrsh)[0]
-            print("below_thrsh_indices")
-            print(below_thrsh_indices)
-            print()
+            #print("below_thrsh_indices")
+            #print(below_thrsh_indices)
+            #print()
             below_thrsh_coords = np_each_coord[below_thrsh_indices]
             print("below_thrsh_coords")
             print(below_thrsh_coords)
             print()
+
+            for adjacent_coord in below_thrsh_coords:
+                self.path_adjacent.points.append(Point(x=x, y=y))
+                self.path_adjacent.points.append(Point(x=adjacent_coord[0], y=adjacent_coord[1]))
+
             # vertex_dict["adjacent"] = below_thrsh_coords.tolist()
             vertex_dict["adjacent"] = below_thrsh_indices.tolist()
             if self.vertex_count not in vertex_dict["adjacent"]:
@@ -71,6 +97,7 @@ class Path_creator():
         self.pose_list.append(vertex_dict)
 
         self.path_pub.publish(self.path)
+        self.adjacent_pub.publish(self.path_adjacent)
         
         # print(self.pose_list)
         # print("callback")
@@ -79,7 +106,7 @@ class Path_creator():
         print("end")
         
     def shutdown(self):
-        self.pose_list[len(self.pose_list) - 1]["adjacent"].append(0)
+        # self.pose_list[len(self.pose_list) - 1]["adjacent"].append(0)
         
         current_time = time.strftime("%Y-%B-%d-%H:%M:%S", time.localtime())
         
